b0f63898860cd45d015b148b86bcc56cfcc95a3c
Elfiawesome23 <elfiawesome23@gmail.com>
2025-08-17 04:57:52 +0800

    updated source testing

--- MelonCryptorCLI/Program.cs
+++ MelonCryptorCLI/Program.cs
@@ -1,24 +1,15 @@
 ﻿using MelonCryptorCLI;
 
-try
+string root = "c:/Users/elfia/OneDrive/Desktop/Melon-Cryptor/";
+
+var mcs = new MelonCryptoService();
+
+mcs.NewVault($"{root}Asset-Testing/vault", "123");
+
+// mcs.OpenVault($"{root}Asset-Testing/vault", "123");
+
+Directory.GetFiles($"{root}Asset-Testing/source").ToList().ForEach(file =>
 {
-	string root = "c:/Users/elfia/OneDrive/Desktop/Melon-Cryptor/";
-
-	var mcs = new MelonCryptoService();
-
-	// mcs.NewVault($"{root}Asset-Testing/vault-debug", "supersecretpassword999");
-
-	mcs.OpenVault($"{root}Asset-Testing/vault-debug", "supersecretpassword999");
-
-	// Directory.GetFiles($"{root}Asset-Testing/source").ToList().ForEach(file =>
-	// {
-	// 	Console.WriteLine($"Stashing file: {file}");
-	// 	mcs.StashFile(file);
-	// });
-
-	mcs.StashFile("c:/Users/elfia/OneDrive/Desktop/Launcher.exe", ["apps", "custom", "launchers"]);
-}
-catch (Exception ex)
-{
-	Console.WriteLine($"Error: {ex.Message}");
-}
+	Console.WriteLine($"Stashing file: {file}");
+	mcs.StashFile(file);
+});
