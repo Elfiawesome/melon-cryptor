8d953157402c38451c5978f9940fa39d65e3b8c2
Elfiawesome23 <elfiawesome23@gmail.com>
2025-08-17 04:11:48 +0800

    Overhaul web page

--- MelonCryptorCLI/Program.cs
+++ MelonCryptorCLI/Program.cs
@@ -6,15 +6,17 @@ try
 
 	var mcs = new MelonCryptoService();
 
-	mcs.NewVault($"{root}Asset-Testing/vault-debug", "supersecretpassword999");
+	// mcs.NewVault($"{root}Asset-Testing/vault-debug", "supersecretpassword999");
 
-	// mcs.OpenVault($"{root}Asset-Testing/vault-debug", "supersecretpassword999");
+	mcs.OpenVault($"{root}Asset-Testing/vault-debug", "supersecretpassword999");
 
-	Directory.GetFiles($"{root}Asset-Testing/source").ToList().ForEach(file =>
-	{
-		Console.WriteLine($"Stashing file: {file}");
-		mcs.StashFile(file);
-	});
+	// Directory.GetFiles($"{root}Asset-Testing/source").ToList().ForEach(file =>
+	// {
+	// 	Console.WriteLine($"Stashing file: {file}");
+	// 	mcs.StashFile(file);
+	// });
+
+	mcs.StashFile("c:/Users/elfia/OneDrive/Desktop/Launcher.exe", ["apps", "custom", "launchers"]);
 }
 catch (Exception ex)
 {
