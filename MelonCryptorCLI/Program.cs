5f57c1cfefe19f78410d6f91c7f5f258cabe8543
Elfiawesome23 <elfiawesome23@gmail.com>
2025-08-17 05:22:29 +0800

    Refactor JSON structure in index.json and update vault access in Program.cs

--- MelonCryptorCLI/Program.cs
+++ MelonCryptorCLI/Program.cs
@@ -4,12 +4,12 @@ string root = "c:/Users/elfia/OneDrive/Desktop/Melon-Cryptor/";
 
 var mcs = new MelonCryptoService();
 
-mcs.NewVault($"{root}Asset-Testing/vault", "123");
+// mcs.NewVault($"{root}Asset-Testing/vault", "123");
 
-// mcs.OpenVault($"{root}Asset-Testing/vault", "123");
+mcs.OpenVault($"{root}Asset-Testing/vault", "123");
 
-Directory.GetFiles($"{root}Asset-Testing/source").ToList().ForEach(file =>
-{
-	Console.WriteLine($"Stashing file: {file}");
-	mcs.StashFile(file);
-});
+// Directory.GetFiles($"{root}Asset-Testing/source").ToList().ForEach(file =>
+// {
+// 	Console.WriteLine($"Stashing file: {file}");
+// 	mcs.StashFile(file);
+// });
